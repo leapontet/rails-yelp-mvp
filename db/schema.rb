4e87e14d547af1cd3ec7af4d4d887cf36dded345
Lea Pontet <lea.pontet@hotmail.fr>
2019-08-14 14:23:23 -0400

    Add category column to restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_153109) do
+ActiveRecord::Schema.define(version: 2019_08_14_182018) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_153109) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "reviews", force: :cascade do |t|
